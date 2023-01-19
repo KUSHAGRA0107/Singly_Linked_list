59923f61048c48f96ea622ad2afc8d5c2408d4fa
KUSHAGRA0107 <116801700+KUSHAGRA0107@users.noreply.github.com>
2023-01-19 13:18:28 +0530

    Add files via upload

---
+++ Middle/01_middle_of_link_list.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node * head=NULL;
+void Insert_at_end(int key)
+{
+    struct node* start=head;
+    struct node *new=(struct node*)malloc(sizeof(struct node));
+    new->data=key;
+    new->next=NULL;
+    if(head==NULL)
+    {
+    head=new;
+    }
+    else{
+    while(start->next!=NULL)
+    {
+        start=start->next;
+    }
+    start->next=new;
+}}
+void Middle()
+{
+    struct node* fast=head;
+    struct node* slow=head;
+    while(fast!=NULL)
+{
+    fast=(fast->next)->next;
+
+    slow=slow->next;
+}
+ printf("The Mid Value Of Link List Is %d ",slow->data);
+}
+
+void PrintList()
+{
+    struct node* start=head;
+    while(start!=NULL)
+    {
+        printf("%d-->",start->data);
+        start=start->next;
+    }
+}
+int main()
+{
+    int key;
+    int n;
+    printf("Enter The Size Of Link List \n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+    printf("Insert The %dth Element ",i+1);
+    scanf("%d",&key);
+    Insert_at_end(key);
+    }
+    PrintList();
+    printf("\n");
+   Middle();   
+}
